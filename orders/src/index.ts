d88fe9d0b0224e01cfd89f320b6fc34d10bcc0aa
Yousuf Hasan <yousuf85@gmail.com>
2020-06-28 20:18:12 +1000

    trigger wf change

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -24,7 +24,7 @@ const start = async () => {
     try{
         await natsWrapper.connect(process.env.NATS_CLUSTER_ID, process.env.NATS_CLIENT_ID, process.env.NATS_URL);
         natsWrapper.client.on('close',()=>{
-            console.log('NATS Connection is closed !!');
+            console.log('NATS Connection is closed.');
             process.exit();
         });
         process.on('SIGINT', ()=> natsWrapper.client.close());
